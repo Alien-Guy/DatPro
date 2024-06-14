eed0d168fe20a76adbf7b32d2d5fffcfc736f14f
Jonas Niermann <152810389+Alien-Guy@users.noreply.github.com>
2024-06-14 12:44:04 +0000

    Generator multiindex erstellt.

--- Aufgabenblätter/Blatt_8/multiindex.py
+++ Aufgabenblätter/Blatt_8/multiindex.py
@@ -3,3 +3,14 @@
 
 #Programm erstellt die Routine multiindex, die alle Indizes zu denen ein Element in der Liste
 #vorkommt ausgibt.
+
+def multiindex(list, element):
+    for p in range(len(list)):
+        if list[p] == element:
+            yield p
+
+# Ab hier Test des Generators
+liste = [0,1,0,1,1,1]
+
+for a in multiindex(liste, 1):
+    print(a)
