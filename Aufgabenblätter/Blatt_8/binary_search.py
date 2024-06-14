08f2b0b807dfd9511fa3240605cb185ce86f3571
Jonas Niermann <152810389+Alien-Guy@users.noreply.github.com>
2024-06-14 13:34:02 +0000

    Routine binary_search erstellt.

--- Aufgabenblätter/Blatt_8/binary_search.py
+++ Aufgabenblätter/Blatt_8/binary_search.py
@@ -1,5 +1,24 @@
+#Aufgabenblatt 7
+#Autor: Jonas Niermann; Matr.-Nr.: 7418131
+
+# Programm definiert die Routine binary_search, die in einer geordneten Liste von Zahlen
+# rekursiv nach einer vorgegebnen Zahl sucht.
+
 def binary_search(data, element, lower, upper):
-# diese Routine rekursiv implementieren...
+    mid = lower + (upper-lower)//2
+    if lower == upper:      # in this case is lower == upper == mid
+        if data[mid] == element:
+            return True
+        else:
+            return False
+    
+    else:
+        if data[mid] == element:
+            return True
+        elif data[mid] > element:
+            return binary_search(data, element, lower, mid-1)
+        elif data[mid] < element:
+            return binary_search(data, element, mid+1, upper)
     pass
 
 
@@ -13,4 +32,3 @@ def search_sorted(data, element):
 l = [0, 1, 2, 3, 4, 4, 6, 7, 8, 9, 10, 12, 12, 13, 14, 15, 16, 17, 18, 19]
 print(f"11: {search_sorted(l, 11)}") # False
 print(f"12: {search_sorted(l, 12)}") # True
-
