47737e731d20470f2245be50a0f089d11a16c1bb
Jonas Niermann <152810389+Alien-Guy@users.noreply.github.com>
2024-06-21 16:22:10 +0000

    Routinen insert, delete, __iter__ und __next__ erstellt.

--- Präsenzübungen/dictionary.py
+++ Präsenzübungen/dictionary.py
@@ -1,3 +1,6 @@
+#Aufgabenblatt 9
+#Autor: Jonas Niermann; Matr.-Nr.: 7418131
+
 #Programm implementiert die Klasse Dictionary manuell in Python-Code.
 
 class Node:
@@ -60,24 +63,44 @@ class Dictionary:
 
 # Fügt (key, value) hinter key_loc ein
     def insert(self, key_loc, key, value):
-# Bitte in den Übungen implementieren
-        pass
+        new_node = Node(key, value)
+        if key_loc == None:
+            new_node.next = self.head.next
+            self.head.next = new_node
+            return
+        node = self.head
+        while node.next != None:
+            node = node.next
+            if key_loc == node.key:
+                new_node.next = node.next
+                node.next = new_node
+                return
+        raise KeyError
 
 # Sucht nach einem Knoten mit key und löscht diesen dann. Löst einen
 # KeyError aus, wenn key nicht in ssl ist
     def delete(self, key):
-# Bitte in den Übungen implementieren
-        pass
+        node = self.head
+        while node.next != None:
+            if key == node.next.key:
+                node.next = node.next.next
+                return
+            node = node.next
+        raise KeyError
 
 # Iterator über die keys, __iter__
     def __iter__(self):
-# Standard-Methode, bitte in den Übugen implementieren
-        pass
+        self.node = self.head
+        return self
 
 # Iterator über die keys, __next__
     def __next__(self):
-# Standard-Methode, bitte in den Übungen implementieren
-        pass
+        if self.node.next != None:
+            self.node = self.node.next
+            current_key = self.node.key
+            return current_key
+        else:
+            raise StopIteration
 
 
 
@@ -93,11 +116,11 @@ try:
     print(f'{d.retrieve("Lea") = }')
 except KeyError:
     print('d.retrieve("Lea") löst KeyError aus')
-#try:
-#    print('d.delete("Lea")')
-#    d.delete("Lea")
-#except KeyError:
-#    print('d.delete("Lea") löst KeyError aus')
+try:
+    print('d.delete("Lea")')
+    d.delete("Lea")
+except KeyError:
+    print('d.delete("Lea") löst KeyError aus')
 
 for item in items:
     print(f"Speichere {item} in d.")
@@ -109,15 +132,15 @@ for item in items:
         print(f'{d.retrieve("Lea") = }')
     except KeyError:
         print('d.retrieve("Lea") löst KeyError aus')
-    #try:
-    #    print('d.delete("Lea")')
-    #    d.delete("Lea")
-    #except KeyError:
-    #    print('d.delete("Lea") löst KeyError aus')
+    try:
+        print('d.delete("Lea")')
+        d.delete("Lea")
+    except KeyError:
+        print('d.delete("Lea") löst KeyError aus')
     print("\n")
 
 # Jetzt den Iterator über die keys in d testen
-#print('Iterator über d testen:')
-#for key in d:
-#    value = d.retrieve(key)
-#    print(f"({key}, {value})")
+print('Iterator über d testen:')
+for key in d:
+    value = d.retrieve(key)
+    print(f"({key}, {value})")
