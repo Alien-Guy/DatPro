8d7b8e3de1d2fac7e97365bc8f77ae82c21db018
Jonas Niermann <152810389+Alien-Guy@users.noreply.github.com>
2024-07-04 16:08:31 +0000

    Von Jakob kopiert.

--- Präsenzübungen/int2d_queues.py
+++ Präsenzübungen/int2d_queues.py
@@ -1,65 +1,73 @@
+import multiprocessing as mp
 import math
 import numpy as np
-import multiprocessing as mp
 
-xmin = -100
-xmax = 100
-ymin = -100
-ymax = 100
-nx = 100001
-ny = 100001
-dx = (xmax-xmin)/(nx-1)
-dy = (ymax-ymin)/(ny-1)
+ymin = -100.0
+ymax =  100.0
+xmin = -100.0
+xmax =  100.0
+nx = 1001
+ny = 1001
+dx = (xmax - xmin)/(nx - 1)
+dy = (ymax - ymin)/(ny - 1)
 N_PROC = 5
 
-def f(x,y):
-    return math.exp(- (x*x + y*y) + y)
+def f(x, y):
+    return math.exp(-(x*x + y*y + y))
 
-def worker(q_work, q_result):
-    sumx = 0
+
+def worker(q_work , q_result):
+    print("work")
+    sumx = 0.
     x_grid = np.linspace(xmin, xmax, nx)
     while True:
         y = q_work.get()
-        if y == None: # Sentinel
+        print("y", y)
+        if y == None:
             return
         for x in x_grid:
             sumx += f(x,y)*dx
         q_result.put(sumx)
 
 def consumer(q_result):
-    result = 0
+    print("cons")
+    result = 0.
     n = 0
     while n < ny-1:
         print("n", n)
-        if n == 5:
+        if n==5:
             break
         g = q_result.get()
         result += g*dy
-        n += 1
-    print(f"Das Integral ist: {result}")
+        n+=1
+    print(f"das Integral ist { result}")
+    return
 
 def main():
+    print("A")
     q_work = mp.Queue()
     q_result = mp.Queue()
     y_grid = np.linspace(ymin, ymax, ny)
-
+    
+    print("B")  
     process = []
-
     process.append(mp.Process(target=consumer, args=(q_result, )))
     process[0].start()
-
-    for n in range(N_PROC):
+    
+    for n in range(1,N_PROC):
         process.append(mp.Process(target=worker, args=(q_work, q_result)))
         process[n].start()
-        
+    print("C")     
     for y in y_grid:
         q_work.put(y)
     for n in range(N_PROC):
         q_work.put(None)
-    
+   
+    print("D")       
     for n in range(N_PROC):
-        process[n].join()
+            process[n].join()
 
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
